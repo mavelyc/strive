bd422a24f5e2617a206ebf0ea64fd08f026c5bb8
Ahmed Fuad Ali <fuad@Ahmeds-MacBook-Pro.local>
2018-09-08 20:44:03 -0400

    experimenting with routing

--- imagetesting/routing.js
+++ imagetesting/routing.js
@@ -4,9 +4,11 @@ var express = require('express');
 var app = express();
 app.set('view engine', 'ejs');
 
+// app listens on port 3000 for requests
+app.listen(3000);
 
 // will be using get requests
-app.get('/about', function(req, res){
-    res.render('about');
+app.get('/api', function(req, res){
+    
 });
 
