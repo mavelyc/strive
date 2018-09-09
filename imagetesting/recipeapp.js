d4a44247703c4d06a47b7b9b42edb77fff3eaa3d
Ahmed Fuad Ali <fuad@Ahmeds-MacBook-Pro.local>
2018-09-09 07:55:36 -0400

    tried fixing issue with routing recipeapp

--- imagetesting/recipeapp.js
+++ imagetesting/recipeapp.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 
 // SETUP APP
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.use('/', express.static(__dirname + '/public'));
